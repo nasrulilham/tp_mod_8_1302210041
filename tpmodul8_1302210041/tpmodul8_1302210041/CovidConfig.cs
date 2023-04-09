fe40c860581e9923a2df720e188cac7cb8154be0
nasrulilham <91175018+nasrulilham@users.noreply.github.com>
2023-04-09 21:12:46 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302210041/tpmodul8_1302210041/CovidConfig.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302210041
+{
+    internal class CovidConfig
+    {
+        public class set
+        {
+            public string satuan_suhu { get; set; }
+            public int batas_hari_demam { get; set; }
+            public string pesan_ditolak { get; set; }
+            public string pesan_diterima { get; set; }
+
+            public set(string satuan_suhu, int batas_hari_demam, 
+                string pesan_ditolak, string pesan_diterima)
+            {
+                this.satuan_suhu = satuan_suhu;
+                this.batas_hari_demam = batas_hari_demam;
+                this.pesan_ditolak = pesan_ditolak;
+                this.pesan_diterima = pesan_diterima;
+            }
+            //setget
+        }
+        public class readWrite
+        {
+            public set config;
+            public const string txt = @"./covid_config.json";
+
+            private void SetDefault()
+            {
+                config = new set(
+                    "celcius", 
+                    14,
+                    "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                    "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+                    );
+            }
+            //method running config
+
+            private void NewConfigFile()
+            {
+                JsonSerializerOptions dokumen = new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                };
+
+                string jsonString = JsonSerializer.Serialize(config, dokumen);
+                File.WriteAllText(txt, jsonString);
+            }
+            //membuat filejson baru
+
+            private set ReadConfigFile()
+            {
+                string readJSON = File.ReadAllText(txt);
+                config = JsonSerializer.Deserialize<set>(readJSON);
+                return config;
+            }
+            //membaca file
+
+            public readWrite()
+            {
+                try
+                {
+                    ReadConfigFile();
+                }
+                catch (Exception)
+                {
+                    SetDefault();
+                    NewConfigFile();
+                }
+            }
+            //try catch, akan mencoba untuk readconfigfile terlebih dahulu bila tidak ada maka akan set default dan membuat yang baru
+
+            public void UbahDerajat()
+            {
+                if (config.satuan_suhu == "celcius")
+                {
+                    config.satuan_suhu = "farenheit";
+                    NewConfigFile();
+                }
+                else
+                {
+                    config.satuan_suhu = "celcius";
+                    NewConfigFile();
+                }
+            }
+            //mengubah ubah satuan celcius dan farenheit
+        }
+    }
+}
--- tpmodul8_1302210041/tpmodul8_1302210041/Program.cs
+++ tpmodul8_1302210041/tpmodul8_1302210041/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using static tpmodul8_1302210041.CovidConfig;
+
+readWrite rw = new readWrite();
+rw.UbahDerajat();
+
+
+Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + rw.config.satuan_suhu);
+Console.WriteLine("(celcius 36,5 - 37,5 atau 97.7 - 99,5)");
+double suhu = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+int hari = Convert.ToInt32(Console.ReadLine());
+//meminta masukan
+
+if ((rw.config.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) && hari < rw.config.batas_hari_demam)
+{
+    Console.WriteLine(rw.config.pesan_diterima);
+}
+else if ((rw.config.satuan_suhu == "farenheit") && suhu >= 97.7 && suhu <= 99.5 && hari < rw.config.batas_hari_demam)
+{
+    Console.WriteLine(rw.config.pesan_diterima);
+}
+else
+{
+    Console.WriteLine(rw.config.pesan_ditolak);
+}
+
+//menentukuan range suhu untuk celcius dan farenheit, bila diluar hal tersbut maka pesan ditolak
